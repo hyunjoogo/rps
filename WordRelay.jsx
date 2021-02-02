0f6cbafaf7fa663438fc7698b8e6e4b9ed8b9438
hyunjoogo <hyunjoogo@gmail.com>
2021-02-02 13:19:18 +0900

    Refactor :+1:  Class형 -> 함수형+Hook으로 변경
    기존 Class형 컴포넌트는 WordRealy-origin.jsx으로 변경

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,55 +1,41 @@
-const React = require('react');
-const { Component } = React
+import React, { useState }from 'react';
 
-class WordRelay extends Component {
-  state= {
-    word : "김현주",
-    value : "",
-    result: "",
-  }
+const WordRelay = () => {
+  const [word, changeWord] = useState("김현주");
+  const [value, changeValue] = useState("");
+  const [result, changeResult] = useState("");
 
-  handelChange = (e) => {
-    this.setState({value:e.target.value});
-  }
 
-  handelSubmit = (e) => {
-    e.preventDefault();
-    const _word = this.state.word
-    const _value = this.state.value
-    if(_word[_word.length -1] === _value[0]) {
-      this.setState({
-        result: "딩동댕", 
-        word:_value , 
-        value:""
-      })
-      this.input.focus();
-    } else {
-      this.setState({
-        result: "땡",
-        value:"",
-      });
-      this.input.focus();
-    }
-  }
-
-  onRefInput = (c) => {
-    this.input = c;
-  }
-  render() {
-    return (
-      <div>
-        <h1>끝말잇기!</h1>
-        <form onSubmit={this.handelSubmit}>
-          <label>
-            <p>시작 단어 : {this.state.word}</p>
-            <input ref={this.onRefInput} value={this.state.value} type="text" onChange={this.handelChange} />
-          </label>
-          <input type="submit" value="제출" />
-        </form>
-        <div>{this.state.result}</div>
-      </div>
-    )
+const handelSubmit = (e) => {
+  e.preventDefault();
+  if (word[word.length -1] === value[0]) {
+    changeResult('딩동댕');
+    changeWord(value);
+    changeValue("");
+  } else {
+    changeResult('땡');
+    changeValue("");
   }
 }
 
-module.exports = WordRelay;
+    return (
+      <div>
+        <h1>끝말잇기!</h1>
+        <form onSubmit={(e) => handelSubmit(e)}>
+          <label>
+            <p>시작 단어 : {word}</p>
+            <input 
+              value={value} 
+              type="text" 
+              onChange={
+                (e)=> changeValue(e.target.value)
+                } />
+          </label>
+          <input type="submit" value="제출" />
+        </form>
+        <div>{result}</div>
+      </div>
+    )
+}
+
+export default WordRelay;
--- client.jsx
+++ client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const WordRelay = require('./WordRelay')
+import WordRelay from './WordRelay'
 
 ReactDom.render(<WordRelay />,document.querySelector("#root"))
