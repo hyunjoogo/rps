028569372c24bf73e06a27d84de75767345a23fe
hyunjoogo <hyunjoogo@gmail.com>
2021-02-05 18:25:05 +0900

    Add : RPS컴포넌트 기초값

--- RPS.jsx
+++ RPS.jsx
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
 
 class RPS extends Component{
-  
+  state = {
+    img : "./src/img/r.png",
+    score : "",
+    result : "",
+  }
+
+  onClickBtn = (item) => {
+
+  }
+
   render() {
-   return (
-     <h1> Hello Joo</h1>
-   )
+    const {result, score, img} = this.state;
+    return(
+      <div>
+        <img src={img} alt="rock" />
+        <div className="btn">
+          <img 
+            src="./src/img/btn_rock.png" 
+            alt="rock-button" 
+            onClick={()=>onClickBtn('rock')} />
+          <img 
+            src="./src/img/btn_paper.png" 
+            alt="paper-button"
+            onClick={()=>onClickBtn('paper')} />
+          <img 
+            src="./src/img/btn_scissors.png" 
+            alt="scissors-button"
+            onClick={()=>onClickBtn('scissors')} />
+        </div>
+        <div>{result}</div>
+        <div>현재 {score} 점</div>
+      </div>
+    )
   }
 }
 
