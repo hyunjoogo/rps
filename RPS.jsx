aa6e31c88e146ef57761821b2435f6f4ae524969
hyunjoogo <hyunjoogo@gmail.com>
2021-02-05 23:50:06 +0900

    Add : 사운드 추가

--- RPS.jsx
+++ RPS.jsx
@@ -4,17 +4,41 @@ const scores = {
   rock : 0,
   paper : 2,
   scissors : 1,
-  "./src/img/r.png" : 0,
-  "./src/img/p.png" : 2,
-  "./src/img/s.png" : 1,
+  "./img/r.png" : 0,
+  "./img/p.png" : 2,
+  "./img/s.png" : 1,
 }
 
 let SPEED = 1000;
 
+const start = new Audio("./sound/start.mp3")
+const win = new Audio("./sound/win.mp3")
+const lose = new Audio("./sound/lose.mp3")
+const draw = new Audio("./sound/draw.mp3")
+const dada = new Audio("./sound/dada.mp3")
+const yes = new Audio("./sound/yes.mp3")
+
+
+start.oncanplaythrough = (event) => {
+    var playedPromise = start.play();
+    if (playedPromise) {
+        playedPromise.catch((e) => {
+          console.log(e)
+          if (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') { 
+            console.log(e.name);
+          }
+        }).then(() => {
+          console.log("playing sound !!!");
+        });
+    }
+}
+
+
+
 class RPS extends Component{  
-  rockUrl = "./src/img/r.png"
-  paperUrl = "./src/img/p.png"
-  scissorsUrl = "./src/img/s.png"
+  rockUrl = "./img/r.png"
+  paperUrl = "./img/p.png"
+  scissorsUrl = "./img/s.png"
 
   state = {
     img : this.rockUrl,
@@ -41,10 +65,15 @@ class RPS extends Component{
   }
 
   componentDidMount() {
-  this.interval = setInterval(this.changeHand, SPEED)
+    this.interval = setInterval(this.changeHand, SPEED)
   }
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  playSound = (sound) => {
+    sound.currentTime = 0;
+    sound.play();
   }
 
   onClickBtn = (choice) => {
@@ -55,22 +84,28 @@ class RPS extends Component{
     const gameScore = myScore - ComScore
     console.log(gameScore, myScore, ComScore);
     if (gameScore === 0) {
-      console.log('비김')
       this.setState({
         result : "다시!"
-      })
-      this.interval = setInterval(this.changeHand, SPEED)
+      });
+      this.playSound(draw);
+      this.timeout = setTimeout(()=>{
+        this.interval = setInterval(this.changeHand, SPEED);
+        this.playSound(start);
+        console.log('setTimeout')
+      }, 1000);
     } else if ([-1, 2].includes(gameScore)) {
       console.log('유저 윈')
       this.setState({
         result : "이겼다!!"
       })
+      this.playSound(win);
       //돌려돌려
     } else if ([1, -2].includes(gameScore)) {
       console.log('컴퓨터 윈')
       this.setState({
         result : "졌다😥😥😥"
       })
+      this.playSound(lose);
       //다시 하시겠습니까?
     }
     // 다시 시작
@@ -78,19 +113,29 @@ class RPS extends Component{
   render() {
     const {result, img} = this.state;
     return(
-      <div>
-        <img src={img} alt="rock" />
+      <div className="wrapper">
+        <div className="motion-area">
+          <img src={img} alt="rock" />
+          <img className="result_draw"
+          src="./img/result_draw.png" alt="draw" />
+          <img className="result_lose"
+          src="./img/result_lose.png" alt="draw" />
+          <img className="result_win_lt"
+          src="./img/result_win_lt.png" alt="draw" />
+          <img className="result_win_rt"
+          src="./img/result_win_rt.png" alt="draw" />
+        </div>
         <div className="btn">
-          <img 
-            src="./src/img/btn_rock.png" 
+          <img className="btn-rock"
+            src="./img/btn_rock.png" 
             alt="rock-button" 
             onClick={()=>this.onClickBtn('rock')} />
-          <img 
-            src="./src/img/btn_paper.png" 
+          <img className="btn-paper"
+            src="./img/btn_paper.png" 
             alt="paper-button"
             onClick={()=>this.onClickBtn('paper')} />
-          <img 
-            src="./src/img/btn_scissors.png" 
+          <img className="btn-scisscors"
+            src="./img/btn_scissors.png" 
             alt="scissors-button"
             onClick={()=>this.onClickBtn('scissors')} />
         </div>
@@ -102,18 +147,3 @@ class RPS extends Component{
 }
 
 export default RPS;
-
-// const {img} = this.state;
-// if (img === "./src/img/r.png") {
-//   this.setState({
-//     img: "./src/img/p.png"
-//   })
-// } else if (img === "./src/img/p.png") {
-//   this.setState({
-//     img: "./src/img/s.png"
-//   })
-// } else if (img === "./src/img/s.png") {
-//   this.setState({
-//     img: "./src/img/r.png"
-//   })
-// }
